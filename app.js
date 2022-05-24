11e3167ab228f40959e10659f1bc03633974fd03
junyeon <jun981008@naver.com>
2022-05-24 14:05:03 +0900

    array

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-alert("hi!");
-ㅇ브라카타브라
+/* const를 제일 많이 사용 let은 중요한 업데이트만 사용
+const는 항상 let은 가끔 var는 절대 사용 안 함 */
+const dayOfWeek = ["mon", "tue", "wed", "thu", "sat", "sun"];
+
+console.log(dayOfWeek);
