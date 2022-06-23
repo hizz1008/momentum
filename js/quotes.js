78cc3d792798900145e1ca8adf528abc0b3fac63
junyeon <jun981008@naver.com>
2022-06-23 21:53:29 +0900

    123

--- js/quotes.js
+++ js/quotes.js
@@ -1,20 +1,20 @@
 const quotes = [
-    {
-        quote:"원빈 현빈 강동원 방준연",
-        author:"방준연"
-},
-{
-    quote:"원빈 현빈 강동원 방준연",
-    author:"방준연"
-},
-{
-    quote:"원빈 현빈 강동원 방준연",
-    author:"방준연"
-},
-{
-    quote:"원빈 현빈 강동원 방준연",
-    author:"방준연"
-}
+  {
+    quote: "BTS 봉준호 손흥민 방준연",
+    author: "Let`s Go!",
+  },
+  {
+    quote: "아이언맨 배트맨 스파이더맨 침착맨",
+    author: "Let`s Go!",
+  },
+  {
+    quote: "KFC 맥도날드 버거킹 석봉토스트",
+    author: "Let`s Go!",
+  },
+  {
+    quote: "CIA FBI CSI 떡잎마을 방범대",
+    author: "Let`s Go!",
+  },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
---
+++ js/todo.js
@@ -0,0 +1,47 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector("#todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function painToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  painToDo(newTodo);
+  saveToDo();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(painToDo);
+}
